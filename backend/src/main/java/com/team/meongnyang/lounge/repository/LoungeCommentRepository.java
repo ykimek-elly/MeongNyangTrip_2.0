efee5ba85bd89b29d413b46302c168703bafcca9
dkgnpd <dkgnpd7@gmail.com>
2026-03-26 14:51:46 +0900

    fix: LoungeCommentRepository createdAt → commentId 정렬로 수정

--- backend/src/main/java/com/team/meongnyang/lounge/repository/LoungeCommentRepository.java
+++ backend/src/main/java/com/team/meongnyang/lounge/repository/LoungeCommentRepository.java
@@ -2,9 +2,8 @@ package com.team.meongnyang.lounge.repository;
 
 import com.team.meongnyang.lounge.entity.LoungeComment;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface LoungeCommentRepository extends JpaRepository<LoungeComment, Long> {
-    List<LoungeComment> findByPost_PostIdOrderByCreatedAtAsc(Long postId);
+    List<LoungeComment> findByPost_PostIdOrderByCommentIdAsc(Long postId);
 }
